f3b3f9e1e1a02b1cb2df1d86a22f777c13497fe5
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-05-12 23:15:50 +0300

    CryptoCrazyRxMVVM improvements added

---
+++ Model/Crypto.swift
@@ -0,0 +1,13 @@
+//
+//  Crypto.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by Aleyna Işıkdağlılar on 9.05.2024.
+//
+
+import Foundation
+
+struct Crypto: Decodable {
+    var currency: String
+    var price: String
+}
